48d980555f8207fdd1a3a5f7238ef75e014d0a21
Boris Vargas <borisvargaspaucara@gmail.com>
2021-08-25 09:31:42 -0400

    second commit

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,10 +1,5 @@
 
 export var GLOBAL = {
-    // locationOrigin: 'http://adf.bvargas.com:7101',
-    // reportOrigin: 'http://ec2-3-212-185-116.compute-1.amazonaws.com:8080',
-    // urlAdf: '/DiaModDiaGAM-RESTWebService-context-root/rest/v1/',
-    // urlRep: '/ServerReports/Reportes',
-    // baseHref: '/DiaModDiaGAM-DiaModDiaGAMViewController-context-root/faces/Dia/DiaGAM/NG/dia-gam',
     cIdioma: 'ESP',
     cPropietario: 'DAZ',
     skin: 'light'
